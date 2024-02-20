ca1ddeb6820a4a784b4423d6acf9955f1876f0bb
Jiadong Liu <liujiadong111@gmail.com>
2024-02-20 15:28:37 -0800

    0.4.2 update files

---
+++ data/SQL/data_sh_immigration_global.sql
@@ -0,0 +1,18 @@
+create table shanghai_immigration_global
+(
+    日期     date null,
+    日本     int  null,
+    新加坡   int  null,
+    泰国     int  null,
+    韩国     int  null,
+    美国     int  null,
+    加拿大   int  null,
+    英国     int  null,
+    法国     int  null,
+    德国     int  null,
+    意大利   int  null,
+    俄罗斯   int  null,
+    澳大利亚 int  null,
+    新西兰   int  null
+);
+
